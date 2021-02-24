7cecb4558d93f8949cf92e232a20b5be5857e7c0
Ian Bell <ian.bell@nist.gov>
2021-02-24 09:40:04 -0500

    tabs -> 4space

--- include/teqp/derivs.hpp
+++ include/teqp/derivs.hpp
@@ -3,42 +3,43 @@
 template <typename TType, typename ContainerType, typename FuncType>
 typename std::enable_if<is_container<ContainerType>::value, typename ContainerType::value_type>::type
 caller(const FuncType& f, TType T, const ContainerType& rho) {
-	return f(T, rho);
+    return f(T, rho);
 }
 
+/// Given a 
 template <typename TType, typename ContainerType, typename FuncType>
 typename std::enable_if<is_container<ContainerType>::value, typename ContainerType::value_type>::type
 deriv1(const FuncType& f, TType T, const ContainerType& rho) {
-	double h = 1e-100;
-	return f(std::complex<TType>(T, h), rho).imag() / h;
+    double h = 1e-100;
+    return f(std::complex<TType>(T, h), rho).imag() / h;
 }
 
 template <typename TType, typename ContainerType, typename FuncType>
 typename std::enable_if<is_container<ContainerType>::value, typename ContainerType::value_type>::type
 deriv2(const FuncType& f, TType T, const ContainerType& rho) {
-	double h = 1e-100;
-	using comtype = std::complex<ContainerType::value_type>;
-	std::valarray<comtype> rhocom(rho.size());
-	rhocom[0] = comtype(rho[0], h);
-	for (auto i = 1; i < rho.size(); ++i) {
-		rhocom[i] = comtype(rho[i], 0.0);
-	}
-	return f(T, rhocom).imag() / h;
+    double h = 1e-100;
+    using comtype = std::complex<ContainerType::value_type>;
+    std::valarray<comtype> rhocom(rho.size());
+    rhocom[0] = comtype(rho[0], h);
+    for (auto i = 1; i < rho.size(); ++i) {
+        rhocom[i] = comtype(rho[i], 0.0);
+    }
+    return f(T, rhocom).imag() / h;
 }
 
 template <typename TType, typename ContainerType, typename FuncType>
 typename std::enable_if<is_container<ContainerType>::value, typename ContainerType::value_type>::type
 deriv3(const FuncType& f, TType T, const ContainerType& rho) {
-	double h = 1e-100;
-	using comtype = std::complex<ContainerType::value_type>;
-	std::valarray<comtype> rhocom(rho.size());
-	rhocom[0] = comtype(rho[0], 0.0);
-	rhocom[1] = comtype(rho[1], h);
-	return f(T, rhocom).imag() / h;
+    double h = 1e-100;
+    using comtype = std::complex<ContainerType::value_type>;
+    std::valarray<comtype> rhocom(rho.size());
+    rhocom[0] = comtype(rho[0], 0.0);
+    rhocom[1] = comtype(rho[1], h);
+    return f(T, rhocom).imag() / h;
 }
 
 template<typename Model, typename TType, typename RhoType>
 auto build_Psir_Hessian(const Model& model, const TType T, const RhoType& rho) {
-	// Double derivatives in each component's concentration
+    // Double derivatives in each component's concentration
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,71 +11,71 @@
 #include "teqp/core.hpp"
 
 void test_vdW() {
-	volatile double T = 298.15;
-	auto rho = 3.0;
-	auto R = get_R_gas<double>();
+    volatile double T = 298.15;
+    auto rho = 3.0;
+    auto R = get_R_gas<double>();
 
-	double Omega_b = 1.0 / 8, Omega_a = 27.0 / 64;
-	double Tcrit = 150.687, pcrit = 4863000.0; // Argon
-	double b = Omega_b * R * Tcrit / pcrit;
-	double ba = Omega_b / Omega_a / Tcrit / R;
-	double a = b / ba;
+    double Omega_b = 1.0 / 8, Omega_a = 27.0 / 64;
+    double Tcrit = 150.687, pcrit = 4863000.0; // Argon
+    double b = Omega_b * R * Tcrit / pcrit;
+    double ba = Omega_b / Omega_a / Tcrit / R;
+    double a = b / ba;
 
-	auto vdW = vdWEOS1(a, b);
+    auto vdW = vdWEOS1(a, b);
 
-	auto t2 = std::chrono::steady_clock::now();
-	volatile auto pp = vdW.p(T, 1 / rho);
-	auto t3 = std::chrono::steady_clock::now();
-	std::cout << std::chrono::duration<double>(t3 - t2).count() << " from p(T,v)" << std::endl;
+    auto t2 = std::chrono::steady_clock::now();
+    volatile auto pp = vdW.p(T, 1 / rho);
+    auto t3 = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration<double>(t3 - t2).count() << " from p(T,v)" << std::endl;
 
-	const std::valarray<double> rhovec = { rho, 0.0 };
+    const std::valarray<double> rhovec = { rho, 0.0 };
 
-	auto t21 = std::chrono::steady_clock::now();
-	
-	auto Psir = vdW.Psir(T, rhovec);
-	auto dPsirdrho0 = rhovec[0] * deriv2([&vdW](const auto& T, const auto& rhovec) { return vdW.Psir(T, rhovec); }, T, rhovec);
-	auto dPsirdrho1 = rhovec[1] * deriv3([&vdW](const auto& T, const auto& rhovec) { return vdW.Psir(T, rhovec); }, T, rhovec);
-	auto pfromderiv = rho * R * T - Psir + dPsirdrho0 + dPsirdrho1;
+    auto t21 = std::chrono::steady_clock::now();
+    
+    auto Psir = vdW.Psir(T, rhovec);
+    auto dPsirdrho0 = rhovec[0] * deriv2([&vdW](const auto& T, const auto& rhovec) { return vdW.Psir(T, rhovec); }, T, rhovec);
+    auto dPsirdrho1 = rhovec[1] * deriv3([&vdW](const auto& T, const auto& rhovec) { return vdW.Psir(T, rhovec); }, T, rhovec);
+    auto pfromderiv = rho * R * T - Psir + dPsirdrho0 + dPsirdrho1;
 
-	auto t31 = std::chrono::steady_clock::now();
-	std::cout << std::chrono::duration<double>(t31 - t21).count() << " from isochoric" << std::endl;
-	std::cout << pfromderiv / pp - 1 << std::endl;
+    auto t31 = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration<double>(t31 - t21).count() << " from isochoric" << std::endl;
+    std::cout << pfromderiv / pp - 1 << std::endl;
 }
 
 void test_vdwMix() {
-	// Argon + Xenon
-	std::valarray<double> Tc_K = { 150.687, 289.733 };
-	std::valarray<double> pc_Pa = { 4863000.0, 5842000.0 };
-	vdWEOS<double> vdW(Tc_K, pc_Pa);
+    // Argon + Xenon
+    std::valarray<double> Tc_K = { 150.687, 289.733 };
+    std::valarray<double> pc_Pa = { 4863000.0, 5842000.0 };
+    vdWEOS<double> vdW(Tc_K, pc_Pa);
 
-	double T = 298.15;
-	auto rho = 3.0;
-	auto R = get_R_gas<double>();
-	auto rhotot = rho;
+    double T = 298.15;
+    auto rho = 3.0;
+    auto R = get_R_gas<double>();
+    auto rhotot = rho;
 
-	const std::valarray<double> rhovec = { rho, 0.0 };
+    const std::valarray<double> rhovec = { rho/2, rho/2 };
 
-	auto t2 = std::chrono::steady_clock::now();
+    auto t2 = std::chrono::steady_clock::now();
 
-	auto Psir = vdW.Psir(T, rhovec);
-	auto dPsirdrho0 = rhovec[0] * deriv2([&vdW, rhotot](const auto& T, const auto& rhovec) { return vdW.Psir(T, rhovec); }, T, rhovec);
-	auto dPsirdrho1 = rhovec[1] * deriv3([&vdW, rhotot](const auto& T, const auto& rhovec) { return vdW.Psir(T, rhovec); }, T, rhovec);
-	auto pfromderiv = rho*R*T - Psir + dPsirdrho0 + dPsirdrho1;
-	{
-		auto term0 = rhovec[0] * deriv2([&vdW, rhotot](const auto& T, const auto& rhovec) { return vdW.alphar(T, rhovec); }, T, rhovec);
-		auto term1 = rhovec[1] * deriv3([&vdW, rhotot](const auto& T, const auto& rhovec) { return vdW.alphar(T, rhovec); }, T, rhovec);
-		auto pr = (term0 + term1)*rhotot*R*T;
-		auto pfromderiv2 = rho*R*T + pr;
-		auto err2 = pfromderiv / pfromderiv2 - 1;
-		int err = 0;
-	}
+    auto Psir = vdW.Psir(T, rhovec);
+    auto dPsirdrho0 = rhovec[0] * deriv2([&vdW, rhotot](const auto& T, const auto& rhovec) { return vdW.Psir(T, rhovec); }, T, rhovec);
+    auto dPsirdrho1 = rhovec[1] * deriv3([&vdW, rhotot](const auto& T, const auto& rhovec) { return vdW.Psir(T, rhovec); }, T, rhovec);
+    auto pfromderiv = rho*R*T - Psir + dPsirdrho0 + dPsirdrho1;
+    {
+        auto term0 = rhovec[0] * deriv2([&vdW, rhotot](const auto& T, const auto& rhovec) { return vdW.alphar(T, rhovec); }, T, rhovec);
+        auto term1 = rhovec[1] * deriv3([&vdW, rhotot](const auto& T, const auto& rhovec) { return vdW.alphar(T, rhovec); }, T, rhovec);
+        auto pr = (term0 + term1)*rhotot*R*T;
+        auto pfromderiv2 = rho*R*T + pr;
+        auto err2 = pfromderiv / pfromderiv2 - 1;
+        int err = 0;
+    }
 
-	auto t3 = std::chrono::steady_clock::now();
-	std::cout << std::chrono::duration<double>(t3 - t2).count() << " from isochoric (mix) " << std::endl;
+    auto t3 = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration<double>(t3 - t2).count() << " from isochoric (mix) " << std::endl;
 }
 
 int main(){
-	test_vdW();
-	test_vdwMix();
-	return EXIT_SUCCESS;
+    test_vdW();
+    test_vdwMix();
+    return EXIT_SUCCESS;
 }
