cfb2c9608b9e8df9051f7e6cfe9fae206301cb28
Ian Bell <ian.bell@nist.gov>
2021-02-25 13:49:40 -0500

    Multi-fluid works, gives same results as CoolProp for methane + ethane in double precision
    Lots of testing remains for other numerical types

--- include/teqp/models/multifluid.hpp
+++ include/teqp/models/multifluid.hpp
@@ -3,6 +3,7 @@
 #include "json.hpp"
 #include <Eigen/Dense>
 #include <fstream>
+#include <string>
 
 template<typename EOSCollection>
 class CorrespondingStatesContribution {
@@ -39,7 +40,7 @@ public:
         resulttype alphar = 0.0;
         auto N = molefracs.size();
         for (auto i = 0; i < N; ++i) {
-            for (auto j = 0; j < N; ++j) {
+            for (auto j = i+1; j < N; ++j) {
                 alphar = alphar + molefracs[i] * molefracs[j] * F(i,j) * funcs[i][j].alphar(tau, delta);
             }
         }
@@ -117,7 +118,7 @@ public:
 
         auto N = z.size();
         MoleFractions::value_type sum1 = 0.0;
-        for (auto i = 0; i < N - 1; ++i) {
+        for (auto i = 0; i < N; ++i) {
             sum1 = sum1 + square(z[i]) * Yc[i];
         }
         
@@ -132,14 +133,23 @@ public:
     }
 
     static auto get_BIPdep(const nlohmann::json& collection, const std::vector<std::string>& components) {
+
+        // convert string to upper case
+        auto toupper = [](const std::string s){ auto data = s; std::for_each(data.begin(), data.end(), [](char& c) { c = ::toupper(c); }); return data;};
+
+        std::string comp0 = toupper(components[0]);
+        std::string comp1 = toupper(components[1]);
         for (auto& el : collection) {
-            if (components[0] == el["Name1"] && components[1] == el["Name2"]) {
+            std::string name1 = toupper(el["Name1"]);
+            std::string name2 = toupper(el["Name2"]);
+            if (comp0 == name1 && comp1 == name2) {
                 return el;
             }
-            if (components[0] == el["Name2"] && components[1] == el["Name1"]) {
+            if (comp0 == name2 && comp1 == name1) {
                 return el;
             }
         }
+        throw std::invalid_argument("Can't match this binary pair");
     }
     static auto get_binary_interaction_double(const nlohmann::json& collection, const std::vector<std::string>& components) {
         auto el = get_BIPdep(collection, components);
@@ -192,8 +202,14 @@ public:
             F(i, i) = 0.0;
             for (auto j = i + 1; j < N; ++j) {
                 auto el = get_BIPdep(collection, { components[i], components[j] });
-                F(i, j) = el["F"];
-                F(j, i) = el["F"];
+                if (el.empty()) {
+                    F(i, j) = 0.0;
+                    F(j, i) = 0.0;
+                }
+                else{
+                    F(i, j) = el["F"];
+                    F(j, i) = el["F"];
+                }   
             }
         }
         return F;
@@ -201,6 +217,221 @@ public:
     template<typename MoleFractions> auto get_Tr(const MoleFractions& molefracs) const { return Y(molefracs, Tc, betaT, YT); }
     template<typename MoleFractions> auto get_rhor(const MoleFractions& molefracs) const { return 1.0 / Y(molefracs, vc, betaV, Yv); }
 };
+
+class MultiFluidDepartureFunction {
+public:
+    enum class types { NOTSETTYPE, GERG2004, GaussianExponential };
+private:
+    types type = types::NOTSETTYPE;
+public:
+    Eigen::ArrayXd n, t, d, c, l, eta, beta, gamma, epsilon;
+
+    void set_type(const std::string& kind) {
+        if (kind == "GERG-2004" || kind == "GERG-2008") {
+            type = types::GERG2004;
+        }
+        else if (kind == "Gaussian+Exponential") {
+            type = types::GaussianExponential;
+        }
+        else {
+            throw std::invalid_argument("Bad type:" + kind);
+        }
+    }
+
+    template<typename TauType, typename DeltaType>
+    auto alphar(const TauType& tau, const DeltaType& delta) const {
+        switch (type) {
+        case (types::GaussianExponential):
+            return (n * pow(tau, t) * pow(delta, d) * exp(-c * pow(delta, l)) * exp(-eta * (delta - epsilon).square() - beta * (tau - gamma).square())).sum();
+        case (types::GERG2004):
+            return (n * pow(tau, t) * pow(delta, d) * exp(-eta * (delta - epsilon).square() - beta * (delta - gamma))).sum();
+        default:
+            throw - 1;
+        }
+    }
+};
+
+auto get_departure_function_matrix(const std::string& coolprop_root, const nlohmann::json& BIPcollection, const std::vector<std::string>& components) {
+
+    // Allocate the matrix with default models
+    std::vector<std::vector<MultiFluidDepartureFunction>> funcs(2); for (auto i = 0; i < funcs.size(); ++i) { funcs[i].resize(funcs.size()); }
+
+    auto depcollection = nlohmann::json::parse(std::ifstream(coolprop_root + "/dev/mixtures/mixture_departure_functions.json"));
+
+    auto get_departure_function = [&depcollection](const std::string& Name) {
+        for (auto& el : depcollection) {
+            if (el["Name"] == Name) { return el; }
+        }
+        throw std::invalid_argument("Bad argument");
+    };
+
+    for (auto i = 0; i < funcs.size(); ++i) {
+        for (auto j = i + 1; j < funcs.size(); ++j) {
+            auto BIP = MultiFluidReducingFunction::get_BIPdep(BIPcollection, { components[i], components[j] });
+            auto function = BIP["function"];
+            if (!function.empty()) {
+
+                auto info = get_departure_function(function);
+                auto N = info["n"].size();
+
+                auto toeig = [](const std::vector<double>& v) -> Eigen::ArrayXd { return Eigen::Map<const Eigen::ArrayXd>(&(v[0]), v.size()); };
+                auto eigorempty = [&info, &toeig, &N](const std::string& name) -> Eigen::ArrayXd {
+                    if (!info[name].empty()) {
+                        return toeig(info[name]);
+                    }
+                    else {
+                        return Eigen::ArrayXd::Zero(N);
+                    }
+                };
+
+                MultiFluidDepartureFunction f;
+                f.set_type(info["type"]);
+                f.n = toeig(info["n"]);
+                f.t = toeig(info["t"]);
+                f.d = toeig(info["d"]);
+
+                f.eta = eigorempty("eta");
+                f.beta = eigorempty("beta");
+                f.gamma = eigorempty("gamma");
+                f.epsilon = eigorempty("epsilon");
+
+                Eigen::ArrayXd c(f.n.size()), l(f.n.size()); c.setZero();
+                if (info["l"].empty()) {
+                    // exponential part not included
+                    l.setZero();
+                }
+                else {
+                    l = toeig(info["l"]);
+                    // l is included, use it to build c; c_i = 1 if l_i > 0, zero otherwise
+                    for (auto i = 0; i < c.size(); ++i) {
+                        if (l[i] > 0) {
+                            c[i] = 1.0;
+                        }
+                    }
+                }
+
+                f.l = l;
+                f.c = c;
+                funcs[i][j] = f;
+                funcs[j][i] = f;
+                int rr = 0;
+            }
+        }
+    }
+    return funcs;
+}
+
+class MultiFluidEOS {
+public:
+    enum class types { NOTSETTYPE, GERG2004, GaussianExponential };
+private:
+    types type = types::NOTSETTYPE;
+public:
+    Eigen::ArrayXd n, t, d, c, l, eta, beta, gamma, epsilon;
+
+    void allocate(int N) {
+        auto go = [&N](Eigen::ArrayXd &v){ v.resize(N); v.setZero(); };
+        go(n); go(t); go(d); go(l); go(c); go(eta); go(beta); go(gamma); go(epsilon);
+    }
+
+    //void set_type(const std::string& kind) {
+    //    if (kind == "GERG-2004" || kind == "GERG-2008") {
+    //        type = types::GERG2004;
+    //    }
+    //    else if (kind == "Gaussian+Exponential") {
+    //        type = types::GaussianExponential;
+    //    }
+    //    else {
+    //        throw std::invalid_argument("Bad type:" + kind);
+    //    }
+    //}
+
+    template<typename TauType, typename DeltaType>
+    auto alphar(const TauType& tau, const DeltaType& delta) const {
+        return (n * pow(tau, t) * pow(delta, d) * exp(-c * pow(delta, l)) * exp(-eta * (delta - epsilon).square() - beta * (tau - gamma).square())).sum();
+        /*case (types::GERG2004):
+            return (n * pow(tau, t) * pow(delta, d) * exp(-eta * (delta - epsilon).square() - beta * (delta - gamma))).sum();
+        default:
+            throw - 1;
+        }*/
+    }
+};
+
+auto get_EOS(const std::string& coolprop_root, const std::string& name) 
+{
+    using namespace nlohmann;
+    auto j = json::parse(std::ifstream(coolprop_root + "/dev/fluids/" + name + ".json"));
+    auto alphar = j["EOS"][0]["alphar"];
+
+    auto ncoeff = 0;
+
+    const std::vector<std::string> allowable_types = {"ResidualHelmholtzPower", "ResidualHelmholtzGaussian"};
+    auto isallowed = [&allowable_types](const std::string &name){ for (auto &a : allowable_types){ if (name == a){return true;};} return false;};
+    for (auto& term : alphar) {
+        std::string type = term["type"];
+        if (!isallowed(type)){
+            throw std::invalid_argument("Bad type:" + type);
+        }
+        else{
+            ncoeff += term["n"].size();
+        }
+    }
+    
+    MultiFluidEOS eos; 
+    eos.allocate(ncoeff); // Allocate arrays to the right size, fill with zero
+    
+    auto toeig = [](const std::vector<double>& v) -> Eigen::ArrayXd { return Eigen::Map<const Eigen::ArrayXd>(&(v[0]), v.size()); };
+    
+    auto offset = 0;
+    for (auto &term: alphar){
+        auto N = term["n"].size();
+
+        auto eigorzero = [&term, &toeig, &N](const std::string& name) -> Eigen::ArrayXd {
+            if (!term[name].empty()) {
+                return toeig(term[name]);
+            }
+            else {
+                return Eigen::ArrayXd::Zero(N);
+            }
+        };        
+
+        eos.n.segment(offset, N) = eigorzero("n");
+        eos.t.segment(offset, N) = eigorzero("t");
+        eos.d.segment(offset, N) = eigorzero("d");
+        eos.eta.segment(offset, N) = eigorzero("eta");
+        eos.beta.segment(offset, N) = eigorzero("beta");
+        eos.gamma.segment(offset, N) = eigorzero("gamma");
+        eos.epsilon.segment(offset, N) = eigorzero("epsilon");
+
+        Eigen::ArrayXd c(N), l(N); c.setZero();
+        if (term["l"].empty()) {
+            // exponential part not included
+            l.setZero();
+        }
+        else {
+            l = toeig(term["l"]);
+            // l is included, use it to build c; c_i = 1 if l_i > 0, zero otherwise
+            for (auto i = 0; i < c.size(); ++i) {
+                if (l[i] > 0) {
+                    c[i] = 1.0;
+                }
+            }
+        }
+        eos.c.segment(offset, N) = c;
+        eos.l.segment(offset, N) = l;
+
+        offset += N;
+    }
+    return eos;
+}
+
+auto get_EOSs(const std::string& coolprop_root, const std::vector<std::string>& names) {
+    std::vector<MultiFluidEOS> EOSs;
+    for (auto& name : names) {
+        EOSs.emplace_back(get_EOS(coolprop_root, name));
+    }
+    return EOSs;
+}
 
 class DummyEOS {
 public:
--- src/multifluid.cpp
+++ src/multifluid.cpp
@@ -5,16 +5,11 @@ auto build_multifluid_model(const std::vector<std::string>& components) {
     using namespace nlohmann;
     std::string coolprop_root = "C:/Users/ihb/Code/CoolProp";
     auto BIPcollection = json::parse(std::ifstream(coolprop_root + "/dev/mixtures/mixture_binary_pairs.json"));
-    
-    std::vector<std::vector<DummyEOS>> funcs(2); for (auto i = 0; i < funcs.size(); ++i) { funcs[i].resize(funcs.size()); }
-    std::vector<DummyEOS> EOSs(components.size());
-
-    auto f = DummyEOS();
-    auto fd = f.alphar(1.1, 1.1);
-    auto fi = f.alphar(1,1);
 
     auto [Tc, vc] = MultiFluidReducingFunction::get_Tcvc(coolprop_root, components);
     auto F = MultiFluidReducingFunction::get_F_matrix(BIPcollection, components);
+    auto funcs = get_departure_function_matrix(coolprop_root, BIPcollection, components);
+    auto EOSs = get_EOSs(coolprop_root,  components);
     auto [betaT, gammaT, betaV, gammaV] = MultiFluidReducingFunction::get_BIP_matrices(BIPcollection, components);
 
     auto redfunc = MultiFluidReducingFunction(betaT, gammaT, betaV, gammaV, Tc, vc);
@@ -27,8 +22,8 @@ auto build_multifluid_model(const std::vector<std::string>& components) {
 }
 
 int main(){
-    test_dummy();
-    auto model = build_multifluid_model({ "Methane", "Ethane" });
+    //test_dummy();
+    auto model = build_multifluid_model({ "methane", "ethane" });
     std::valarray<double> rhovec = { 1.0, 2.0 };
     auto alphar = model.alphar(300.0, rhovec);
     return EXIT_SUCCESS;
