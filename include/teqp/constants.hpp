354610429d88d644b577edae5717777413622d23
Ian Bell <ian.bell@nist.gov>
2021-05-13 11:49:29 -0400

    Just the product

--- include/teqp/constants.hpp
+++ include/teqp/constants.hpp
@@ -7,5 +7,5 @@ template<typename NumType>
 const auto get_R_gas() {
 	const double k_B = 1.380649e-23; ///< Boltzmann constant
 	const double N_A = 6.02214076e23; ///< Avogadro's number
-	return forceeval(static_cast<NumType>(N_A*k_B));
+	return N_A*k_B;
 };
