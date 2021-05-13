949b6a960911688f7519da4dd31ae7b0001a2073
Ian Bell <ian.bell@nist.gov>
2021-05-13 11:45:46 -0400

    Fixed the reference issue, maybe?

--- include/teqp/constants.hpp
+++ include/teqp/constants.hpp
@@ -4,8 +4,8 @@ const double N_A = 6.02214076e23; ///< Avogadro's number
 
 ///< Gas constant, according to CODATA 2019, in the given number type
 template<typename NumType>
-const NumType get_R_gas() {
+const auto get_R_gas() {
 	const double k_B = 1.380649e-23; ///< Boltzmann constant
 	const double N_A = 6.02214076e23; ///< Avogadro's number
-	return N_A*k_B;
+	return forceeval(static_cast<NumType>(N_A*k_B));
 };
