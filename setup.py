c16cb879355573e071343c6228e0d3dc5db609e3
Ian Bell <ian.bell@nist.gov>
2021-08-03 19:02:28 -0400

    version 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 with open('interface/teqpversion.hpp','w') as fpver:
     fpver.write(f'#include <string>\nconst std::string TEQPVERSION = "{VERSION}";')
 
