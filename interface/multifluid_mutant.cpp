0ee8ea7f7deba8de89e6320ec7bab8daa930e112
Ian Bell <ian.bell@nist.gov>
2021-08-06 10:34:58 -0400

    Some docs for the mutant wrapping

--- interface/multifluid_mutant.cpp
+++ interface/multifluid_mutant.cpp
@@ -5,16 +5,24 @@
 
 void add_multifluid_mutant(py::module& m) {
 
+    // A typedef for the base model
     using MultiFluid = decltype(build_multifluid_model(std::vector<std::string>{"", ""}, "", ""));
-    {
-        m.def("build_multifluid_mutant", &build_multifluid_mutant<MultiFluid>);
-        using RedType = std::decay_t<decltype(MultiFluid::redfunc)>;
-        using DepType = std::decay_t<decltype(MultiFluid::dep)>;
-        using BIPmod = std::decay_t<MultiFluidAdapter<RedType, DepType, MultiFluid>>;
-        auto wMFBIP = py::class_<BIPmod>(m, "MultiFluidMutant")
-            .def("set_meta", [](BIPmod& c, const std::string &s) { return c.set_meta(s); })
-            .def("get_meta", [](const BIPmod& c) { return c.get_meta(); })
-            ;
-        add_derivatives<BIPmod>(m, wMFBIP);
-    }
+    
+    // Wrap the function for generating a multifluid mutant
+    m.def("build_multifluid_mutant", &build_multifluid_mutant<MultiFluid>);
+
+    // The reducing function type and the departure function types are the same
+    // as the base model
+    using RedType = std::decay_t<decltype(MultiFluid::redfunc)>;
+    using DepType = std::decay_t<decltype(MultiFluid::dep)>;
+
+    // Typedef for modified mutant type
+    using BIPmod = std::decay_t<MultiFluidAdapter<RedType, DepType, MultiFluid>>;
+
+    // Define python wrapper of the mutant class
+    auto wMFBIP = py::class_<BIPmod>(m, "MultiFluidMutant")
+        .def("set_meta", [](BIPmod& c, const std::string &s) { return c.set_meta(s); })
+        .def("get_meta", [](const BIPmod& c) { return c.get_meta(); })
+        ;
+    add_derivatives<BIPmod>(m, wMFBIP);
 }
