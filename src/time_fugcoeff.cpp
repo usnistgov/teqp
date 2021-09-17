a5db456bcfc8f3b5384b0a3dfb75dc43cb6d73fd
Ian Bell <ian.bell@nist.gov>
2021-09-17 11:41:51 -0400

    Enable ln(phi) test on linux

--- src/time_fugcoeff.cpp
+++ src/time_fugcoeff.cpp
@@ -14,13 +14,14 @@
 #include <numeric>
 
 #include "teqp/models/multifluid.hpp"
+#include "teqp/derivs.hpp"
 
 int main()
 {
     // You may need to change this path to suit your installation
     // Note: forward-slashes are recommended.
-    std::string path = "C:/Program Files (x86)/REFPROP";
-    std::string DLL_name = "REFPRP64.dll";
+    std::string path = std::getenv("RPPREFIX");
+    std::string DLL_name = "";
 
     // Load the shared library and set up the fluid
     std::string err;
