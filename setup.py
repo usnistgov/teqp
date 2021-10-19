11111a53a6cda0d4b9477d956f564784a708afa1
Ian Bell <ian.bell@nist.gov>
2021-10-19 11:25:27 -0400

    Bump to dev version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-VERSION = '0.2'
+VERSION = '0.2.1dev'
 with open('interface/teqpversion.hpp','w') as fpver:
     fpver.write(f'#include <string>\nconst std::string TEQPVERSION = "{VERSION}";')
 
