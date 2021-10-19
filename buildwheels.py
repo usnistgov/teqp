d1eafbd0d9c600a5add999148b4911d134760ef1
Ian Bell <ian.bell@nist.gov>
2021-10-19 11:09:42 -0400

    Make the wheel building script a bit more powerful

--- buildwheels.py
+++ buildwheels.py
@@ -20,24 +20,33 @@ else:
         if k not in os.environ:
             raise KeyError(f'You must set the twine environment variable {k}')
 
-for pyver in ['3.7','3.8','3.9']:
-    # Build the wheel if it is not already built
-    abbrv = pyver.replace('.', '')
-    if not glob.glob(f'teqp*cp{abbrv}*.whl'):
+def build():
+    for pyver in ['3.7','3.8','3.9']:
+        # Build the wheel if it is not already built
+        abbrv = pyver.replace('.', '')
         condaenv = f'conda-{pyver}'
         subprocess.check_call(f'conda create -y -n {condaenv} python={pyver}', shell=True)
         subprocess.check_call(f'conda activate {condaenv} && python -m pip install -U pip wheel', shell=True)
         try:
-            subprocess.check_call(f'conda activate {condaenv} && python -m pip -vvv --use-feature=in-tree-build wheel .', shell=True)
-        except:
-            pass
+            subprocess.check_call(f'conda activate {condaenv} && python -m pip -vvv wheel .', shell=True)
+        except BaseException as be:
+            print(be)
         finally:
             subprocess.check_call(f'conda env remove -y -n {condaenv}',shell=True)
 
-# Upload wheels
-if userc:
-    twine_call = f'twine upload --config-file pypirc *.whl'
-else:
-    twine_call = f'twine upload *.whl'
+def upload():
+    # Upload wheels
+    if userc:
+        twine_call = f'twine upload --config-file pypirc *.whl'
+    else:
+        twine_call = f'twine upload *.whl'
+    subprocess.check_call(twine_call, shell=True)
 
-subprocess.check_call(twine_call, shell=True)
+steps = ['build','upload']
+if not any([k in sys.argv for k in steps]):
+    raise ValueError("need to pass at least one step to do: '+str(steps)")
+
+if 'build' in sys.argv:
+    build()
+if 'upload' in sys.argv:
+    upload()
