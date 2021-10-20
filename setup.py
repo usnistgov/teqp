3f8c6b08eb77fb4470a7583cdda9d248ac72b128
Ian Bell <ian.bell@nist.gov>
2021-10-20 16:41:02 -0400

    Also get __version__ back out

--- setup.py
+++ setup.py
@@ -71,6 +71,8 @@ from .teqp import *
 def get_datapath():
     """Get the absolute path to the folder containing the root of multi-fluid data"""
     return os.path.abspath(os.path.dirname(__file__)+"/fluiddata")
+
+from .teqp import __version__
 '''
 
 def prepare():
