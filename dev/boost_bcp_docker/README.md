df5aa2ade01cb940ce16ace1800612615ae0a26c
Ian Bell <ian.bell@nist.gov>
2021-10-20 14:00:10 -0400

    Working example of the use of odeint from boost

--- dev/boost_bcp_docker/README.md
+++ dev/boost_bcp_docker/README.md
@@ -1,7 +1,7 @@
 # INFO
 
-Just a tiny docker container used to extract the necessary parts of boost with the use of the tool bcp
+Just a tiny docker container used to extract the necessary parts of ``boost`` with the use of the tool ``bcp``
 
 To run: ``docker-compose up --build``
 
-The output will be a ``.tar.gz`` file with the bits and pieces needed to include in the ``output`` folder.
+The output will be a ``.tar.gz`` file in this folder with the bits and pieces needed to include.
---
+++ src/test_boost_odeint.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <valarray>
+
+#include <boost/numeric/odeint/stepper/controlled_runge_kutta.hpp>
+#include <boost/numeric/odeint/stepper/runge_kutta_cash_karp54.hpp>
+
+/* The type of container used to hold the state vector */
+typedef std::vector< double > state_type;
+
+void xprime( const state_type &x , state_type &dxdt , const double /* t */ )
+{
+    const double gam = 0.15;
+    dxdt[0] = x[1];
+    dxdt[1] = -x[0] - gam*x[1];
+}
+
+int main(int /* argc */ , char** /* argv */ )
+{
+    using namespace boost::numeric::odeint;
+
+    // Typedefs for the types
+    typedef runge_kutta_cash_karp54< state_type > error_stepper_type;
+    typedef controlled_runge_kutta< error_stepper_type > controlled_stepper_type;
+
+    // Define the tolerances
+    double abs_err = 1.0e-10 , rel_err = 1.0e-6 , a_x = 1.0 , a_dxdt = 1.0;
+    controlled_stepper_type controlled_stepper( default_error_checker< double , range_algebra , default_operations >( abs_err , rel_err , a_x , a_dxdt ) );
+
+    state_type x0 = {1.0, 0.0}; // Starting point
+
+    // First integrate, adaptively, until you get as close to the end as you can
+    double t = 0, dt = 0.001, tmax = 100.0;
+    auto write = [&]() { std::cout << t << " " << x0[0] << "," << x0[1] << std::endl;  };
+    for (auto i = 0; t < tmax; ++i){
+        if (t + dt > tmax) { break; }
+        write();
+        auto status = controlled_stepper.try_step(xprime, x0, t, dt);
+        // The state is mutable, so here is where you could modify the solution
+    }
+    write();
+    double dtfinal = tmax - t;
+    auto status = controlled_stepper.try_step(xprime, x0, t, dtfinal);
+    write();
+}
