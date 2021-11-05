75d4530f3ae29d056cd11f1a6f3b11a551fb1acc
Ian Bell <ian.bell@nist.gov>
2021-11-05 09:27:51 -0400

    what() is not permitted to throw in base

--- include/teqp/exceptions.hpp
+++ include/teqp/exceptions.hpp
@@ -7,7 +7,7 @@ public:
     const int code;
     const std::string msg;
     teqpcException(int code, const std::string& msg) : code(code), msg(msg) {};
-    const char *what() const override {
+    const char *what() const noexcept override {
         return msg.c_str();
     }
 };
