cfb72f265e300ef48f9f8b4b89047728d9a749dd
Ian Bell <ian.bell@nist.gov>
2021-11-17 08:32:10 -0500

    Turn lambdas to functions

--- include/teqp/types.hpp
+++ include/teqp/types.hpp
@@ -69,13 +69,13 @@ namespace teqp {
         }
     }
 
-    auto toeig = [](const std::vector<double>& v) -> Eigen::ArrayXd { return Eigen::Map<const Eigen::ArrayXd>(&(v[0]), v.size()); };
+    inline auto toeig (const std::vector<double>& v) -> Eigen::ArrayXd { return Eigen::Map<const Eigen::ArrayXd>(&(v[0]), v.size()); }
 
-    auto all_same_length = [](const nlohmann::json& j, const std::vector<std::string>& ks) {
+    inline auto all_same_length(const nlohmann::json& j, const std::vector<std::string>& ks) {
         std::set<decltype(j[0].size())> lengths;
         for (auto k : ks) { lengths.insert(j[k].size()); }
         return lengths.size() == 1;
-    };
+    }
 
     class Timer {
     private:
