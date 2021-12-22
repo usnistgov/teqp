d4ee8d7ab60bce4ebe54b73f93f1b1b50fc6d531
Ian Bell <ian.bell@nist.gov>
2021-12-22 17:12:39 -0500

    Fix the javascript build to make sure exceptions are handled properly

--- interface/js/build.sh
+++ interface/js/build.sh
@@ -2,13 +2,17 @@
 
 mkdir /bld
 cd /bld
-cmake /src -DCMAKE_TOOLCHAIN_FILE=/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DTEQP_NO_PYTHON=ON -DTEQP_JAVASCRIPT_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DTEQP_SNIPPETS=ON
+cmake /src -DCMAKE_TOOLCHAIN_FILE=/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DTEQP_NO_PYTHON=ON -DTEQP_JAVASCRIPT_MODULE=ON -DTEQP_JAVASCRIPT_HTML=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DTEQP_SNIPPETS=ON
 cmake --build . --target emtest_catch_exception2
 cmake --build . --target bench
 cmake --build . --target catch_tests
-cp emtest_catch_exception2.* /src
-cp bench.* /src
-cp catch_tests.* /src
 
-node --experimental-wasm-eh emtest_catch_exception2.js
+#node --experimental-wasm-eh emtest_catch_exception2.js
+#node --experimental-wasm-eh bench.js
+#node --experimental-wasm-eh catch_tests.js
+
+cp emtest_catch_exception2.* /src/interface/js
+cp bench.* /src/interface/js
+cp catch_tests.* /src/interface/js
+
 exit 0
