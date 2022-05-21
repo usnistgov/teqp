501150acb2daa0d4f04843a683df27abcb6d20e2
Ian Bell <ian.bell@nist.gov>
2022-05-21 18:56:52 -0400

    Fix two wrappers that don't have model as first argument
    Problem: compile-time time arguments remapped to runtime arguments, in future the runtime arguments should come after the model

--- interface/pybind11_wrapper.hpp
+++ interface/pybind11_wrapper.hpp
@@ -42,8 +42,8 @@ void add_derivatives(py::module &m, Wrapper &cls) {
     using vd = VirialDerivatives<Model, double, Eigen::Array<double,Eigen::Dynamic,1>>;
     m.def("get_B2vir", &vd::get_B2vir, py::arg("model"), py::arg("T"), py::arg("molefrac").noconvert());
     cls.def("get_B2vir", &vd::get_B2vir, py::arg("T"), py::arg("molefrac").noconvert());
-    cls.def("get_Bnvir", &vd::get_Bnvir_runtime, py::arg("Nderiv"), py::arg("T"), py::arg("molefrac").noconvert());
-    cls.def("get_dmBnvirdTm", &vd::get_dmBnvirdTm_runtime, py::arg("Nderiv"), py::arg("NTderiv"), py::arg("T"), py::arg("molefrac").noconvert());
+    cls.def("get_Bnvir", [](const Model& m, const int Nderiv, const double T, const RAX molefrac) { return vd::get_Bnvir_runtime(Nderiv, m, T, molefrac); }, py::arg("Nderiv"), py::arg("T"), py::arg("molefrac").noconvert());
+    cls.def("get_dmBnvirdTm", [](const Model& m, const int Nderiv, const int NTderiv, const double T, const RAX molefrac) { return vd::get_dmBnvirdTm_runtime(Nderiv, NTderiv, m, T, molefrac); }, py::arg("Nderiv"), py::arg("NTderiv"), py::arg("T"), py::arg("molefrac").noconvert());
     
     m.def("get_B12vir", &vd::get_B12vir, py::arg("model"), py::arg("T"), py::arg("molefrac").noconvert());
 
