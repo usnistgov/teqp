106acf74aee1f7c42cab14b9603b9e40b76cd658
Ian Bell <ian.bell@nist.gov>
2022-08-18 09:36:08 -0400

    Work on C++ interface
    The use of void* in the interface is not my favorite solution...

--- interface/CPP/teqpcpp.cpp
+++ interface/CPP/teqpcpp.cpp
@@ -1,15 +1,28 @@
+// Pulls in the AllowedModels variant
+#include "teqp/models/fwd.hpp"
+
 #include "teqpcpp.hpp"
 #include "teqp/derivs.hpp"
 #include "teqp/algorithms/critical_tracing.hpp"
 
-double teqp::cppinterface::get_Arxy(const teqp::AllowedModels&model, const int NT, const int ND, const double T, const double rho, const Eigen::ArrayXd &molefracs){
+double teqp::cppinterface::get_Ar01(const void* modelptr, const double T, const double rho, const Eigen::ArrayXd& molefracs) {
+    const teqp::AllowedModels& model = *static_cast<const teqp::AllowedModels*>(modelptr);
+    return std::visit([&](const auto& model) {
+        using tdx = teqp::TDXDerivatives<decltype(model), double, std::decay_t<decltype(molefracs)>>;
+        return tdx::get_Arxy<0, 1, ADBackends::autodiff>(model, T, rho, molefracs);
+        }, model);
+}
+
+double teqp::cppinterface::get_Arxy(const void* modelptr, const int NT, const int ND, const double T, const double rho, const Eigen::ArrayXd &molefracs){
+    const teqp::AllowedModels& model = *static_cast<const teqp::AllowedModels*>(modelptr);
     return std::visit([&](const auto& model) {
         using tdx = teqp::TDXDerivatives<decltype(model), double, std::decay_t<decltype(molefracs)>>;
         return tdx::get_Ar(NT, ND, model, T, rho, molefracs);
     }, model);
 }
 
-nlohmann::json teqp::cppinterface::trace_critical_arclength_binary(const teqp::AllowedModels& model, const double T0, const Eigen::ArrayXd &rhovec0) {
+nlohmann::json teqp::cppinterface::trace_critical_arclength_binary(const void* modelptr, const double T0, const Eigen::ArrayXd &rhovec0) {
+    const teqp::AllowedModels& model = *static_cast<const teqp::AllowedModels*>(modelptr);
     return std::visit([&](const auto& model) {
         using crit = teqp::CriticalTracing<decltype(model), double, std::decay_t<decltype(rhovec0)>>;
         return crit::trace_critical_arclength_binary(model, T0, rhovec0, "");
--- interface/CPP/teqpcpp.hpp
+++ interface/CPP/teqpcpp.hpp
@@ -1,15 +1,14 @@
 #pragma once 
 
-#include <valarray>
-
-// Pulls in the AllowedModels variant
-#include "teqp/models/fwd.hpp"
+#include <Eigen/Dense>
+#include "nlohmann/json.hpp"
 
 namespace teqp {
-	namespace cppinterface {
+    namespace cppinterface {
 
-		// Wrapper functions
-		double get_Arxy(const AllowedModels& model, const int NT, const int ND, const double T, const double rho, const Eigen::ArrayXd& molefracs);
-		nlohmann::json trace_critical_arclength_binary(const AllowedModels& model, const double T0, const Eigen::ArrayXd& rhovec0);
-	}
+        // Wrapper functions
+        double get_Ar01(const void* model, const double T, const double rho, const Eigen::ArrayXd& molefracs); 
+        double get_Arxy(const void* model, const int NT, const int ND, const double T, const double rho, const Eigen::ArrayXd& molefracs);
+        nlohmann::json trace_critical_arclength_binary(const void* model, const double T0, const Eigen::ArrayXd& rhovec0);
+    }
 }
