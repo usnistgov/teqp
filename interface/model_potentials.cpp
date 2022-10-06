6b3ca6268a1198dfdb87d986fc7638fc607716cb
Ian Bell <ian.bell@nist.gov>
2022-10-06 09:14:57 -0400

    Add EOS of Kataoka for EXP-6 EOS

--- interface/model_potentials.cpp
+++ interface/model_potentials.cpp
@@ -8,14 +8,24 @@
 #include "pybind11_wrapper.hpp"
 
 #include "teqp/models/squarewell.hpp"
+#include "teqp/models/exp6.hpp"
 
 using namespace teqp;
 
 void add_model_potentials(py::module& m) {
     using namespace teqp::squarewell;
-    
-    auto cls = py::class_<EspindolaHeredia2009>(m, "SW_EspindolaHeredia2009")
-        .def(py::init<double>(), py::arg("lambda_"))
+    {
+        auto cls = py::class_<EspindolaHeredia2009>(m, "SW_EspindolaHeredia2009")
+            .def(py::init<double>(), py::arg("lambda_"))
         ;
-    add_derivatives<EspindolaHeredia2009>(m, cls);
+        add_derivatives<EspindolaHeredia2009>(m, cls);
+    }
+    
+    using namespace teqp::exp6;
+    {
+        auto cls = py::class_<Kataoka1992>(m, "EXP6_Kataoka1992")
+            .def(py::init<double>(), py::arg("alpha"))
+        ;
+        add_derivatives<Kataoka1992>(m, cls);
+    }
 }
---
+++ src/tests/catch_test_EXP6.cxx
@@ -0,0 +1,32 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+
+using Catch::Approx;
+
+#include "teqp/algorithms/critical_pure.hpp"
+
+#include "teqp/models/exp6.hpp"
+#include "teqp/derivs.hpp"
+#include <Eigen/Dense>
+
+using namespace teqp;
+
+TEST_CASE("simple evaluation of s^+ for EXP6 EOS", "[exp6]")
+{
+    
+    auto model = exp6::Kataoka1992(16.20689655172410);
+    std::valarray<double> z = {1.0};
+    
+    auto Tstar = 1.26503685842324, rhostar = 0.32024121151801;
+    auto ar = model.alphar(Tstar, rhostar, z);
+    
+    using id = IsochoricDerivatives<decltype(model)>;
+    auto rhovec = (Eigen::ArrayXd(1) << rhostar).finished();
+    auto splus = id::get_splus(model, Tstar, rhovec);
+    
+    auto splus_target = 0.93797012263388;
+    CHECK(splus_target == Approx(splus));
+    
+    auto cr = teqp::get_pure_critical_conditions_Jacobian(model, Tstar, rhostar);
+    CHECK(std::isfinite(std::get<1>(cr)(0,0)));
+}
