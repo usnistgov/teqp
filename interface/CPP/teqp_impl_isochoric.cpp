65fe6f132464f84a73df90e6a6465eabfad29544
Ian Bell <ian.bell@nist.gov>
2023-04-18 09:59:06 -0400

    Fix bug with returned map not copying to Array
    Closes #41

--- interface/CPP/teqp_impl_isochoric.cpp
+++ interface/CPP/teqp_impl_isochoric.cpp
@@ -17,10 +17,10 @@ ARXY_args
 // Here XMacros are used to create functions like get_Ar01n, get_Ar02n, ....
 #define X(i) \
 EArrayd MI::get_Ar0 ## i ## n(const double T, const double rho, const REArrayd& molefracs) const { \
-    return std::visit([&](const auto& model) { \
+    return std::visit([&](const auto& model) -> EArrayd { \
         using tdx = teqp::TDXDerivatives<decltype(model), double, REArrayd>; \
         auto vals = tdx::template get_Ar0n<i>(model, T, rho, molefracs); \
-        return Eigen::Map<Eigen::ArrayXd>(&(vals[0]), vals.size());\
+        return Eigen::Map<Eigen::ArrayXd>(&(vals[0]), vals.size()); \
     }, m_model); \
 }
 AR0N_args
