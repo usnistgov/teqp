38acfeacb3244ba0c20d25203ff60f6f25297acd
Ian Bell <ian.bell@nist.gov>
2023-05-09 11:05:26 -0400

    Add runtime check in rootfinding

--- include/teqp/algorithms/rootfinding.hpp
+++ include/teqp/algorithms/rootfinding.hpp
@@ -18,6 +18,9 @@ auto NewtonRaphson(Callable f, const Inputs& args, double tol) {
         Eigen::ArrayXd v = J.colPivHouseholderQr().solve(-r0.matrix());
         x += v;
         auto err = r0.matrix().norm();
+        if (!std::isfinite(err)){
+            throw std::invalid_argument("err is now NaN");
+        }
         if (err < tol) {
             break;
         }
