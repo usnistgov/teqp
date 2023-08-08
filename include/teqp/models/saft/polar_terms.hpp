4bc303a2659d1e24be2aa78352a57511bc0842ab
Ian Bell <ian.bell@nist.gov>
2023-08-08 11:39:12 -0400

    Add cross-polar term of Vrabec and Gross

--- include/teqp/models/saft/polar_terms.hpp
+++ include/teqp/models/saft/polar_terms.hpp
@@ -87,6 +87,41 @@ auto get_JQQ_3ijk(const Eta& eta, const MType& mijk) {
     return forceeval(summer);
 }
 
+
+/// Eq. 16 from Vrabec and Gross, JPCB, 2008. doi: 10.1021/jp072619u
+template <typename Eta, typename MType, typename TType>
+auto get_JDQ_2ij(const Eta& eta, const MType& mij, const TType& Tstarij) {
+    static Eigen::ArrayXd a_0 = (Eigen::ArrayXd(4) << 0.6970950, -0.6335541, 2.9455090, -1.4670273).finished();
+    static Eigen::ArrayXd a_1 = (Eigen::ArrayXd(4) << -0.6734593, -1.4258991, 4.1944139, 1.0266216).finished();
+    static Eigen::ArrayXd a_2 = (Eigen::ArrayXd(4) << 0.6703408, -4.3384718, 7.2341684, 0).finished();
+
+    static Eigen::ArrayXd b_0 = (Eigen::ArrayXd(4) << -0.4840383, 1.9704055, -2.1185727, 0).finished();
+    static Eigen::ArrayXd b_1 = (Eigen::ArrayXd(4) << 0.6765101, -3.0138675, 0.4674266, 0).finished();
+    static Eigen::ArrayXd b_2 = (Eigen::ArrayXd(4) << -1.1675601, 2.1348843, 0, 0).finished();
+    
+    std::common_type_t<Eta, MType, TType> summer = 0.0;
+    for (auto n = 0; n < 4; ++n){
+        auto anij = a_0[n] + (mij-1)/mij*a_1[n] + (mij-1)/mij*(mij-2)/mij*a_2[n]; // Eq. 18
+        auto bnij = b_0[n] + (mij-1)/mij*b_1[n] + (mij-1)/mij*(mij-2)/mij*b_2[n]; // Eq. 19
+        summer += (anij + bnij/Tstarij)*pow(eta, n);
+    }
+    return forceeval(summer);
+}
+
+
+/// Eq. 17 from Vrabec and Gross, JPCB, 2008. doi: 10.1021/jp072619u
+template <typename Eta, typename MType>
+auto get_JDQ_3ijk(const Eta& eta, const MType& mijk) {
+    static Eigen::ArrayXd c_0 = (Eigen::ArrayXd(4) << 7.846431, 33.42700, 4.689111, 0).finished();
+    static Eigen::ArrayXd c_1 = (Eigen::ArrayXd(4) << -20.72202, -58.63904, -1.764887, 0).finished();
+    std::common_type_t<Eta, MType> summer = 0.0;
+    for (auto n = 0; n < 4; ++n){
+        auto cnijk = c_0[n] + (mijk-1)/mijk*c_1[n]; // Eq. 20
+        summer += cnijk*pow(eta, n);
+    }
+    return forceeval(summer);
+}
+
 /***
  * \brief The dipolar contribution given in Gross and Vrabec
  */
@@ -268,6 +303,116 @@ public:
     }
 };
 
+
+/***
+ * \brief The cross dipolar-quadrupolar contribution from Vrabec and Gross
+ *  doi: https://doi.org/10.1021/jp072619u
+ *
+ */
+class DipolarQuadrupolarContributionVrabecGross {
+private:
+    const Eigen::ArrayXd m, sigma_Angstrom, epsilon_over_k, mustar2, nmu, Qstar2, nQ;
+    
+public:
+    const bool has_a_polar;
+    DipolarQuadrupolarContributionVrabecGross(
+      const Eigen::ArrayX<double> &m,
+      const Eigen::ArrayX<double> &sigma_Angstrom,
+      const Eigen::ArrayX<double> &epsilon_over_k,
+      const Eigen::ArrayX<double> &mustar2,
+      const Eigen::ArrayX<double> &nmu,
+      const Eigen::ArrayX<double> &Qstar2,
+      const Eigen::ArrayX<double> &nQ
+    ) : m(m), sigma_Angstrom(sigma_Angstrom), epsilon_over_k(epsilon_over_k), mustar2(mustar2), nmu(nmu), Qstar2(Qstar2), nQ(nQ), has_a_polar(Qstar2.cwiseAbs().sum() > 0 || mustar2.cwiseAbs().sum() > 0) {
+        // Check lengths match
+        if (m.size() != Qstar2.size()){
+            throw teqp::InvalidArgument("bad size of Qstar2");
+        }
+        if (m.size() != nQ.size()){
+            throw teqp::InvalidArgument("bad size of nQ");
+        }
+        if (m.size() != mustar2.size()){
+            throw teqp::InvalidArgument("bad size of mustar2");
+        }
+        if (m.size() != nmu.size()){
+            throw teqp::InvalidArgument("bad size of n");
+        }
+    }
+    DipolarQuadrupolarContributionVrabecGross& operator=( const DipolarQuadrupolarContributionVrabecGross& ) = delete; // non copyable
+    
+    /// Eq. 14 from Vrabec and Gross
+    template<typename TTYPE, typename RhoType, typename EtaType, typename VecType>
+    auto get_alpha2DQ(const TTYPE& T, const RhoType& rhoN_A3, const EtaType& eta, const VecType& mole_fractions) const{
+        const auto& x = mole_fractions; // concision
+        const auto& sigma = sigma_Angstrom; // concision
+        const auto N = mole_fractions.size();
+        std::common_type_t<TTYPE, RhoType, decltype(mole_fractions[0])> summer = 0.0;
+        for (auto i = 0; i < N; ++i){
+            for (auto j = 0; j < N; ++j){
+                auto ninj = nQ[i]*nQ[j];
+                if (ninj > 0){
+                    // Lorentz-Berthelot mixing rules
+                    auto epskij = sqrt(epsilon_over_k[i]*epsilon_over_k[j]);
+                    auto sigmaij = (sigma[i]+sigma[j])/2;
+                    
+                    auto Tstarij = forceeval(T/epskij);
+                    auto mij = std::min(sqrt(m[i]*m[j]), 2.0);
+                    summer += x[i]*x[j]*epsilon_over_k[i]/T*epsilon_over_k[j]/T*POW3(sigma[i])*POW5(sigma[j])/POW5(sigmaij)*ninj*mustar2[i]*Qstar2[j]*get_JDQ_2ij(eta, mij, Tstarij);
+                }
+            }
+        }
+        return forceeval(-static_cast<double>(EIGEN_PI)*POW2(9.0/4.0)*rhoN_A3*summer);
+    }
+    
+    /// Eq. 15 from Vrabec and Gross
+    template<typename TTYPE, typename RhoType, typename EtaType, typename VecType>
+    auto get_alpha3DQ(const TTYPE& T, const RhoType& rhoN_A3, const EtaType& eta, const VecType& mole_fractions) const{
+        const auto& x = mole_fractions; // concision
+        const auto& sigma = sigma_Angstrom; // concision
+        const std::size_t N = mole_fractions.size();
+        std::common_type_t<TTYPE, RhoType, decltype(mole_fractions[0])> summer = 0.0;
+        for (std::size_t i = 0; i < N; ++i){
+            for (std::size_t j = 0; j < N; ++j){
+                for (std::size_t k = 0; k < N; ++k){
+                    auto ninjnk = nQ[i]*nQ[j]*nQ[k];
+                    if (ninjnk > 0){
+                        // Lorentz-Berthelot mixing rules for sigma
+                        auto sigmaij = (sigma[i]+sigma[j])/2;
+                        auto sigmaik = (sigma[i]+sigma[k])/2;
+                        auto sigmajk = (sigma[j]+sigma[k])/2;
+                        
+                        auto mijk = std::min(pow(m[i]*m[j]*m[k], 1.0/3.0), 2.0);
+                        double alpha_GV = 1.19374; // Table 3
+                        auto polars = mustar2[i]*mustar2[j]*Qstar2[k] + alpha_GV*mustar2[i]*Qstar2[j]*Qstar2[k];
+                        summer += x[i]*x[j]*x[k]*epsilon_over_k[i]/T*epsilon_over_k[j]/T*epsilon_over_k[k]/T*POW4(sigma[i]*sigma[j]*sigma[k])/POW2(sigmaij*sigmaik*sigmajk)*ninjnk*polars*get_JDQ_3ijk(eta, mijk);
+                    }
+                }
+            }
+        }
+        return forceeval(-POW2(rhoN_A3)*summer);
+    }
+    
+    /***
+     * \brief Get the cross-polar contribution to \f$ \alpha = A/(NkT) \f$
+     */
+    template<typename TTYPE, typename RhoType, typename EtaType, typename VecType>
+    auto eval(const TTYPE& T, const RhoType& rho_A3, const EtaType& eta, const VecType& mole_fractions) const {
+        auto alpha2 = get_alpha2DQ(T, rho_A3, eta, mole_fractions);
+        auto alpha3 = get_alpha3DQ(T, rho_A3, eta, mole_fractions);
+        auto alpha = forceeval(alpha2/(1.0-alpha3/alpha2));
+        
+        using alpha2_t = decltype(alpha2);
+        using alpha3_t = decltype(alpha3);
+        using alpha_t = decltype(alpha);
+        struct DipolarQuadrupolarContributionTerms{
+            alpha2_t alpha2;
+            alpha3_t alpha3;
+            alpha_t alpha;
+        };
+        return DipolarQuadrupolarContributionTerms{alpha2, alpha3, alpha};
+    }
+};
+
 enum class multipolar_argument_spec {
     TK_rhoNA3_packingfraction_molefractions,
     TK_rhoNm3_rhostar_molefractions
@@ -286,6 +431,9 @@ struct MultipolarContributionGrossVrabecTerms{
     type alpha2QQ;
     type alpha3QQ;
     type alphaQQ;
+    type alpha2DQ;
+    type alpha3DQ;
+    type alphaDQ;
     type alpha;
 };
 
@@ -295,7 +443,7 @@ public:
     
     const std::optional<DipolarContributionGrossVrabec> di;
     const std::optional<QuadrupolarContributionGrossVrabec> quad;
-    // TODO: add cross term
+    const std::optional<DipolarQuadrupolarContributionVrabecGross> diquad;
     
     MultipolarContributionGrossVrabec(
       const Eigen::ArrayX<double> &m,
@@ -306,7 +454,9 @@ public:
       const Eigen::ArrayX<double> &Qstar2,
       const Eigen::ArrayX<double> &nQ)
     : di(((nmu.sum() > 0) ? decltype(di)(DipolarContributionGrossVrabec(m, sigma_Angstrom, epsilon_over_k, mustar2, nmu)) : std::nullopt)),
-      quad(((nQ.sum() > 0) ? decltype(quad)(QuadrupolarContributionGrossVrabec(m, sigma_Angstrom, epsilon_over_k, Qstar2, nQ)) : std::nullopt)) {};
+      quad(((nQ.sum() > 0) ? decltype(quad)(QuadrupolarContributionGrossVrabec(m, sigma_Angstrom, epsilon_over_k, Qstar2, nQ)) : std::nullopt)),
+      diquad(((nQ.sum() > 0) ? decltype(diquad)(DipolarQuadrupolarContributionVrabecGross(m, sigma_Angstrom, epsilon_over_k, mustar2, nmu, Qstar2, nQ)) : std::nullopt))
+    {};
     
     template<typename TTYPE, typename RhoType, typename EtaType, typename VecType>
     auto eval(const TTYPE& T, const RhoType& rho_A3, const EtaType& eta, const VecType& mole_fractions) const {
@@ -326,9 +476,16 @@ public:
             alphaQQ = forceeval(alpha2QQ/(1.0-alpha3QQ/alpha2QQ));
         }
         
-        auto alpha = forceeval(alphaDD + alphaQQ);
+        type alpha2DQ = 0.0, alpha3DQ = 0.0, alphaDQ = 0.0;
+        if (diquad && diquad.value().has_a_polar){
+            alpha2DQ = diquad.value().get_alpha2DQ(T, rho_A3, eta, mole_fractions);
+            alpha3DQ = diquad.value().get_alpha3DQ(T, rho_A3, eta, mole_fractions);
+            alphaDQ = forceeval(alpha2DQ/(1.0-alpha3DQ/alpha2DQ));
+        }
         
-        return MultipolarContributionGrossVrabecTerms<type>{alpha2DD, alpha3DD, alphaDD, alpha2QQ, alpha3QQ, alphaQQ, alpha};
+        auto alpha = forceeval(alphaDD + alphaQQ + alphaDQ);
+        
+        return MultipolarContributionGrossVrabecTerms<type>{alpha2DD, alpha3DD, alphaDD, alpha2QQ, alpha3QQ, alphaQQ, alpha2DQ, alpha3DQ, alphaDQ, alpha};
     }
     
 };
