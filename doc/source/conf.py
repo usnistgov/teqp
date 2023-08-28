99ccc35da608e71ebf452abb08d0254690fcc20e
Jonathan Guyer <guyer@nist.gov>
2023-08-28 18:17:36 -0400

    Remove expensive steps from configuration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,28 +29,6 @@ author = 'Ian Bell'
 import teqp
 release = teqp.__version__
 
-# -- Execute all notebooks --------------------------------------------------
-
-# # Run doxygen (always)
-# if os.path.exists(here+'/_static/'):
-#     shutil.rmtree(here+'/_static/')
-# os.makedirs(here+'/_static')
-# print(f"::debug::here+'/../..' = {here+'/../..'}")
-# subprocess.check_call('doxygen Doxyfile', cwd=here+'/../..', shell=True)
-# 
-# if on_rtd:
-#     # subprocess.check_output(f'jupyter nbconvert --version', shell=True)
-#     for path, dirs, files in os.walk('.'):
-#         for file in files:
-#             if file.endswith('.ipynb') and '.ipynb_checkpoints' not in path:
-#                 subprocess.check_output(f'jupyter nbconvert  --to notebook --output {file} --execute {file}', shell=True, cwd=path)
-#                 # --ExecutePreprocessor.allow_errors=True      (this allows you to allow errors globally, but a raises-exception cell tag is better)
-# 
-# 
-# ### -- Auto-generate API documentation -----------------------------------------
-# 
-# subprocess.check_output(f'sphinx-apidoc -f -o api {os.path.dirname(teqp.__file__)}', shell=True, cwd=here)
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
