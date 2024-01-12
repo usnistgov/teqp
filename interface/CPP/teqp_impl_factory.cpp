7b0e123599206eb01537a82985fae0e6cc190dc6
Ian Bell <ian.bell@nist.gov>
2024-01-12 10:52:42 -0500

    Allow ability to disable validation in C interface
    Closes #83

--- interface/CPP/teqp_impl_factory.cpp
+++ interface/CPP/teqp_impl_factory.cpp
@@ -55,9 +55,12 @@ namespace teqp {
             std::string kind = json.at("kind");
             auto spec = json.at("model");
             
+            // Read in flag to enable/disable validation, if present
+            bool validate_in_json = json.value("validate", true);
+            
             auto itr = pointer_factory.find(kind);
             if (itr != pointer_factory.end()){
-                if (validate){
+                if (validate || validate_in_json){
                     if (model_schema_library.contains(kind)){
                         // This block is not thread-safe, needs a mutex or something
                         JSONValidator validator(model_schema_library.at(kind));
