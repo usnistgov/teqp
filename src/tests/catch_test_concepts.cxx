b8efaae1b5d73e4be8d85f0217ec93a02cc7e433
Ian Bell <ian.bell@nist.gov>
2024-03-01 14:23:18 -0500

    Clean up concepts test

--- src/tests/catch_test_concepts.cxx
+++ src/tests/catch_test_concepts.cxx
@@ -1,26 +1,18 @@
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/catch_approx.hpp>
-#include <catch2/benchmark/catch_benchmark_all.hpp>
-using Catch::Approx;
 
 #include <iostream>
 #include <concepts>
 
-template<typename T>
-concept UsesTauDelta = requires(T t) {
-    { t.has_alphar_taudelta } -> std::same_as<std::true_type&>;
-};
-
-template<typename T>
-concept DoesntUseTauDelta = requires(T t) {
-    { t.has_alphar_taudelta } -> std::same_as<std::false_type&>;
+template<typename T, typename target_type>
+concept TauDeltaDef = requires(T t) {
+    { t.has_alphar_taudelta } -> std::same_as<target_type&>;
 };
 
 void get(auto t){
-    if constexpr (UsesTauDelta<decltype(t)>){
+    if constexpr (TauDeltaDef<decltype(t), std::true_type>){
         std::cout << "Defined and used" << std::endl;
     }
-    else if constexpr (DoesntUseTauDelta<decltype(t)>){
+    else if constexpr (TauDeltaDef<decltype(t), std::false_type>){
         std::cout << "Defined and not used" << std::endl;
     }
     else{
@@ -29,16 +21,15 @@ void get(auto t){
 }
 
 struct Adeftrue{
-    std::false_type has_alphar_taudelta;
-};
-
-struct Bdeffalse{
     std::true_type has_alphar_taudelta;
+};
+struct Adeffalse{
+    std::false_type has_alphar_taudelta;
 };
 struct C{
 };
 
 TEST_CASE("Test some C++20 things", "[C++20]"){
-    static_assert(DoesntUseTauDelta<Adeffalse>);
-    static_assert(UsesTauDelta<Adeftrue>);
+    static_assert(TauDeltaDef<Adeffalse, std::false_type>);
+    static_assert(TauDeltaDef<Adeftrue, std::true_type>);
 }
