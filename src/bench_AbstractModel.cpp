70f5b50ae489662c4bf7d4d69976251c5d9af0cb
Ian Bell <ian.bell@nist.gov>
2024-06-05 14:38:04 -0400

    Paths need fixing for benchmarking of AbstractState

--- src/bench_AbstractModel.cpp
+++ src/bench_AbstractModel.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark_all.hpp>
+#include <catch2/reporters/catch_reporter_event_listener.hpp>
+#include <catch2/reporters/catch_reporter_registrars.hpp>
 
 #include "teqp/cpp/teqpcpp.hpp"
 #include "teqp/cpp/derivs.hpp"
@@ -9,6 +11,20 @@
 #include "teqp/models/GERG/GERG.hpp"
 
 using namespace teqp;
+
+#include "tests/test_common.in"
+
+class testRunListener : public Catch::EventListenerBase {
+public:
+    using Catch::EventListenerBase::EventListenerBase;
+
+    void testRunStarting(Catch::TestRunInfo const&) override {
+        if (!std::filesystem::exists(FLUIDDATAPATH)){
+            throw std::invalid_argument("Tests must be run from the folder where this folder points to a valid location relative to current working directory: " + FLUIDDATAPATH);
+        }
+    }
+};
+CATCH_REGISTER_LISTENER(testRunListener)
 
 TEST_CASE("GERG2008 parts", "[GERG2008]")
 {
@@ -28,9 +44,9 @@ TEST_CASE("multifluid derivatives", "[mf]")
     nlohmann::json j = {
         {"kind", "multifluid"},
         {"model", {
-            {"components", {"../mycp/dev/fluids/Methane.json"}},
-            {"BIP", "../mycp/dev/mixtures/mixture_binary_pairs.json"},
-            {"departure", "../mycp/dev/mixtures/mixture_departure_functions.json"}
+            {"components", {FLUIDDATAPATH+"/dev/fluids/Methane.json"}},
+            {"BIP", FLUIDDATAPATH+"/dev/mixtures/mixture_binary_pairs.json"},
+            {"departure", FLUIDDATAPATH+"/dev/mixtures/mixture_departure_functions.json"}
         }
     }};
     //std::cout << j.dump(2);
@@ -48,8 +64,11 @@ TEST_CASE("multifluid derivatives", "[mf]")
     BENCHMARK("Ar20") {
         return am->get_Ar20(300, 3.0, z);
     };
-    BENCHMARK("get_Ar02n") {
-        return am->get_Ar02n(300, 3.0, z);
+    BENCHMARK("Ar02") {
+        return am->get_Ar02(300, 3.0, z);
+    };
+    BENCHMARK("Ar11") {
+        return am->get_Ar11(300, 3.0, z);
     };
     BENCHMARK("fugacity coefficients") {
         return am->get_fugacity_coefficients(300.0, rhovec);
@@ -76,9 +95,9 @@ TEST_CASE("multifluid derivatives via DerivativeAdapter", "[mf]")
     nlohmann::json j = {
         {"kind", "multifluid"},
         {"model", {
-            {"components", {"../mycp/dev/fluids/Methane.json"}},
-            {"BIP", "../mycp/dev/mixtures/mixture_binary_pairs.json"},
-            {"departure", "../mycp/dev/mixtures/mixture_departure_functions.json"}
+            {"components", {FLUIDDATAPATH+"/dev/fluids/Methane.json"}},
+            {"BIP", FLUIDDATAPATH+"/dev/mixtures/mixture_binary_pairs.json"},
+            {"departure", FLUIDDATAPATH+"/dev/mixtures/mixture_departure_functions.json"}
         }
     }};
     const auto am = teqp::cppinterface::make_model(j);
@@ -129,6 +148,12 @@ TEST_CASE("GERG2008 derivatives", "[GERG2008]")
     };
     BENCHMARK("get_Ar02n") {
         return am->get_Ar02n(300, 3.0, z);
+    };
+    BENCHMARK("get_Ar20n") {
+        return am->get_Ar20n(300, 3.0, z);
+    };
+    BENCHMARK("get_Ar11") {
+        return am->get_Ar11(300, 3.0, z);
     };
     BENCHMARK("fugacity coefficients") {
         return am->get_fugacity_coefficients(300.0, rhovec);
