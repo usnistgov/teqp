7c3eea619676d8cf5b1535541cacbb2424959974
Ian Bell <ian.bell@nist.gov>
2024-06-21 19:06:06 -0400

    generic SAFT is working again

--- interface/CPP/teqp_impl_factory.cpp
+++ interface/CPP/teqp_impl_factory.cpp
@@ -15,6 +15,7 @@ namespace teqp {
         std::unique_ptr<teqp::cppinterface::AbstractModel> make_PCSAFT(const nlohmann::json &);
         std::unique_ptr<teqp::cppinterface::AbstractModel> make_PCSAFTPureGrossSadowski2001(const nlohmann::json &);
         std::unique_ptr<teqp::cppinterface::AbstractModel> make_SOFTSAFT(const nlohmann::json &);
+        std::unique_ptr<teqp::cppinterface::AbstractModel> make_genericSAFT(const nlohmann::json &);
     
         std::unique_ptr<teqp::cppinterface::AbstractModel> make_GERG2004resid(const nlohmann::json &);
         std::unique_ptr<teqp::cppinterface::AbstractModel> make_GERG2008resid(const nlohmann::json &);
@@ -63,6 +64,7 @@ namespace teqp {
             {"PCSAFT", [](const nlohmann::json& spec){ return make_PCSAFT(spec); }},
             {"PCSAFTPureGrossSadowski2001", [](const nlohmann::json& spec){ return make_PCSAFTPureGrossSadowski2001(spec); }},
             {"SoftSAFT", [](const nlohmann::json& spec){ return make_SOFTSAFT(spec); }},
+            {"genericSAFT", [](const nlohmann::json& spec){ return make_genericSAFT(spec); }},
             
             {"GERG2004resid", [](const nlohmann::json& spec){ return make_GERG2004resid(spec);}},
             {"GERG2008resid", [](const nlohmann::json& spec){ return make_GERG2008resid(spec);}},
--- src/tests/catch_test_SAFTgeneric.cxx
+++ src/tests/catch_test_SAFTgeneric.cxx
@@ -3,6 +3,7 @@
 using Catch::Approx;
 #include <catch2/benchmark/catch_benchmark_all.hpp>
 
+#include "teqp/cpp/teqpcpp.hpp"
 #include "teqp/models/saft/genericsaft.hpp"
 
 using namespace teqp;
@@ -30,7 +31,8 @@ TEST_CASE("Benchmark generic PC-SAFT+Association model", "[SAFTgeneric]"){
           "b / m^3/mol": [0.0000145],
           "betaAB": [0.0692],
           "epsAB/kB / J/mol": [2500.7],
-          "molecule_sites": [["e","e","H","H"]]
+          "molecule_sites": [["e","e","H","H"]],
+          "options": {"radial_dist": "CS"}
         }
       }
     }
@@ -55,13 +57,18 @@ TEST_CASE("Benchmark generic PC-SAFT+Association model", "[SAFTgeneric]"){
     }
     )"_json;
     
-    teqp::genericsaft::GenericSAFT saftass(contents);
-    teqp::genericsaft::GenericSAFT saft(contents_PCSAFT);
+    using namespace teqp::saft::genericsaft;
+    GenericSAFT saftass(contents);
+    GenericSAFT saft(contents_PCSAFT);
     teqp::PCSAFT::PCSAFTMixture basesaft = teqp::PCSAFT::PCSAFTfactory(contents_PCSAFT["nonpolar"]["model"]);
     BENCHMARK("Parsing and construction"){
-        return teqp::genericsaft::GenericSAFT(contents);
+        return GenericSAFT(contents);
     };
-//    auto model = teqp::cppinterface::make_model(contents);
+    nlohmann::json maincontents = {
+        {"kind", "genericSAFT"},
+        {"model", contents}
+    };
+    auto model = teqp::cppinterface::make_model(maincontents);
     auto z = (Eigen::ArrayXd(1) << 1.0).finished();
     
     BENCHMARK("alphar w/ assoc."){
@@ -73,14 +80,17 @@ TEST_CASE("Benchmark generic PC-SAFT+Association model", "[SAFTgeneric]"){
     BENCHMARK("alphar w/o assoc."){
         return basesaft.alphar(300.0, 10000.0, z);
     };
-//    BENCHMARK("Ar11"){
-//        return model->get_Ar11(300, 10000, z);
-//    };
-//    BENCHMARK("Ar02"){
-//        return model->get_Ar02(300, 10000, z);
-//    };
-//    BENCHMARK("Ar20"){
-//        return model->get_Ar20(300, 10000, z);
-//    };
+    BENCHMARK("Ar00"){
+        return model->get_Ar00(300, 10000, z);
+    };
+    BENCHMARK("Ar11"){
+        return model->get_Ar11(300, 10000, z);
+    };
+    BENCHMARK("Ar02"){
+        return model->get_Ar02(300, 10000, z);
+    };
+    BENCHMARK("Ar20"){
+        return model->get_Ar20(300, 10000, z);
+    };
 }
 
