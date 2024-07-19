37c7147e51d410316a2d8d5fc5ccb46effde2859
Ian Bell <ian.bell@nist.gov>
2024-07-19 13:53:27 -0400

    Fix some double-> int cast warnings

--- include/teqp/models/model_potentials/2center_ljf.hpp
+++ include/teqp/models/model_potentials/2center_ljf.hpp
@@ -258,7 +258,7 @@ namespace teqp {
 
                 result r = 0.0;
                 for (auto i = 0U; i < c.size(); ++i) {
-                    r = r + c[i] * pow(tau, m[i]) * powi(delta, n[i]) * pow(alpha, o[i]) * exp(p[i] * powi(delta, q[i]));
+                    r = r + c[i] * pow(tau, m[i]) * powi(delta, static_cast<int>(n[i])) * pow(alpha, o[i]) * exp(p[i] * powi(delta, static_cast<int>(q[i])));
                 }
                 return forceeval(r);
             }
