b0294322a282739f8e830e278873bddab2317334
Ian Bell <ian.bell@nist.gov>
2025-02-03 12:21:18 -0500

    Implement test of https://github.com/usnistgov/teqp/issues/162
    Seems to pass

--- src/tests/catch_test_VLLE.cxx
+++ src/tests/catch_test_VLLE.cxx
@@ -69,6 +69,40 @@ TEST_CASE("Test VLLE for nitrogen + ethane for isotherm", "[VLLE]")
     CHECK(rho0s.max() == Approx(19890.1584));
 }
 
+TEST_CASE("Test VLLE for nitrogen + ethane for isotherm with single-point traces", "[VLLE]")
+{
+    // As in the examples in https://doi.org/10.1021/acs.iecr.1c04703
+    std::vector<std::string> names = {"Nitrogen", "Ethane"};
+    using namespace teqp::cppinterface;
+    auto model = make_multifluid_model(names, FLUIDDATAPATH);
+    std::vector<decltype(model)> pures;
+    pures.emplace_back(make_multifluid_model({names[0]}, FLUIDDATAPATH));
+    pures.emplace_back(make_multifluid_model({names[1]}, FLUIDDATAPATH));
+
+    double T = 120.3420;
+    std::vector<nlohmann::json> traces;
+    for (int ipure : {0, 1}){
+
+        // Init at the pure fluid endpoint
+        auto m0 = build_multifluid_model({names[ipure]}, FLUIDDATAPATH);
+        auto pure0 = nlohmann::json::parse(m0.get_meta()).at("pures")[0];
+        auto jancillaries = pure0.at("ANCILLARIES");
+        auto anc = teqp::MultiFluidVLEAncillaries(jancillaries);
+
+        auto rhoLpurerhoVpure = pures[ipure]->pure_VLE_T(T, anc.rhoL(T), anc.rhoV(T), 10);
+        auto rhovecL = (Eigen::ArrayXd(2) << 0.0, 0.0).finished();
+        auto rhovecV = (Eigen::ArrayXd(2) << 0.0, 0.0).finished();
+        rhovecL[ipure] = rhoLpurerhoVpure[0];
+        rhovecV[ipure] = rhoLpurerhoVpure[1];
+        TVLEOptions opt; opt.p_termination = 1e8; opt.crit_termination=1e-4; opt.calc_criticality=true;
+        auto trace = model->trace_VLE_isotherm_binary(T, rhovecL, rhovecV, opt);
+        trace.get_ref<nlohmann::json::array_t&>().resize(1); // Keep only one point
+        traces.push_back(trace);
+    }
+    auto VLLEsoln = VLLE::find_VLLE_T_binary(*model, traces);
+    REQUIRE(VLLEsoln.size() == 0);
+}
+
 TEST_CASE("Test VLLE for nitrogen + ethane for isobar", "[VLLE]")
 {
     // As in the examples in https://doi.org/10.1021/acs.iecr.1c04703
